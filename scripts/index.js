02937a2fed14407d9102bf73347e8d7257c64bc0
AkshithPottigari <akshithpottigari@gmail.com>
2022-08-20 11:48:07 +0530

    made home and shop workable

--- scripts/index.js
+++ scripts/index.js
@@ -21,19 +21,28 @@ let tempUl = document.createElement('ul');
 tempUl.setAttribute('class', 'menu menu-compact backdrop-blur-2xl mt-3 p-2 pl-4 shadow bg-base/30 rounded-box w-52');
 searchDropDown.appendChild(tempUl);
 
+let homePage;
+let shopPage;
+let homeDiv;
+let shopDiv;
 
-
-var products;
+var products=[];
 
 
 let loadingDiv = document.querySelector('#loading');
 
 // Functions
 
-const empty = (arr) => (arr.length = 0);
+const empty = (arr) => (arr.splice(0,arr.length));
+
 const loading = (state) => {
     loadingDiv.style.display = state ? 'visible' : 'none';
 };
+
+const changePage = (page, state) => {
+    // page = state;
+    console.log(`changed page ${page} state ${state}`);
+}
 
 async function onKeyUp(){
     if (searchInput.value != ''){
@@ -51,21 +60,62 @@ async function onKeyUp(){
     }
 }
 
-async function onInit(){
+async function loadHome(){
     loading(true);
+    empty(products);
+    if(shopDiv?.hasChildNodes()){ // clearing the shopdiv if present
+        shopDiv.innerHTML = '';
+    }
+
+    if (homeDiv?.childElementCount > 0){
+        homeDiv.innerHTML = ''; // clearing the homediv if user is clicks on home again
+    }
     await fetch(base_url + '/product').then((res)=> res.json()).then((data)=> {
-        let products_div = document.querySelector('#main_container').appendChild(document.createElement('div'));
+        empty(products);
         products = data;   
-        products_div.setAttribute('class', 'grid p-5 overflow-hidden sm:grid-cols-1 md:grid-cols-3  xl:grid-cols-5 grid-rows-2 gap-2 place-items-center');
+        homeDiv.setAttribute('class', 'grid p-5 overflow-hidden sm:grid-cols-1 md:grid-cols-3  xl:grid-cols-5 grid-rows-2 gap-2 place-items-center');
+        homeDiv.setAttribute('id', 'home');
         for (let product of products){
             let product_card = document.createElement('product-card');
             product_card.setAttribute('data-product', JSON.stringify(product));
-            products_div.appendChild(product_card);
+            homeDiv.appendChild(product_card);
         }
     })
     .then(()=>{
+        changePage(home, true);
+        changePage(shop, false);
         loading(false);
     });
+}
+
+async function loadShop(){
+    empty(products);
+    loading(true);
+    if(homeDiv?.hasChildNodes()){ // clearing the homediv if present
+        homeDiv.innerHTML = '';
+    }
+    if (shopDiv?.childElementCount > 0){ // clearing the shopdiv if user is clicks on shop again
+        shopDiv.innerHTML = '';
+    }
+    await fetch(base_url + '/product').then((res)=> res.json()).then((data)=> {
+        products = data;   
+        shopDiv.setAttribute('class', 'grid p-5 overflow-hidden sm:grid-cols-1 md:grid-cols-3  xl:grid-cols-5 grid-rows-2 gap-2 place-items-center');
+        shopDiv.setAttribute('id', 'home');
+        for (let product of products){
+            let product_card = document.createElement('product-card');
+            product_card.setAttribute('data-product', JSON.stringify(product));
+            shopDiv.appendChild(product_card);
+        }
+    })
+    .then(()=>{
+        changePage(shop, true);
+        changePage(home, false);
+        loading(false);
+    });
+}
+
+async function onInit(){
+    await loadHome();
 }
 
 async function loadSideMenu(){
@@ -77,17 +127,25 @@ async function loadSideMenu(){
     let tempLi = document.createElement('li');
     tempLi.innerHTML = page;
     tempLi.setAttribute('class', 'text-black mb-1 font-semibold cursor-pointer');
-    tempLi.setAttribute('id', page.toLowerCase());
+    tempLi.setAttribute('id', `${page.toLowerCase()}Button`);
     sidemenu.appendChild(tempLi);
   });
-
 }
+
 
 window.onload = function() {
     loadSideMenu();
     onInit();
-}   
 
+    // Declaring pages
+    homePage = document.querySelector('#homeButton');
+    shopPage = document.querySelector('#shopButton');
+    shopPage.addEventListener('click', loadShop);
+    homePage.addEventListener('click', loadHome);
+
+    homeDiv = document.querySelector('#main_container').appendChild(document.createElement('div'));
+    shopDiv = document.querySelector('#main_container').appendChild(document.createElement('div'));
+}   
 
 // Event Listeners
 searchInput.addEventListener('keyup', onKeyUp);
